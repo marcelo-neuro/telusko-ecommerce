ca23d33f7f1fb023b3a33ebbb029f86f2f7eb181
marcelo-neuro <marcelofurla0813@gmail.com>
2026-01-06 14:48:23 -0300

    Feat: ProductRepository created

---
+++ src/main/java/com/marcelo_neuro/telusko_ecommerce/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.marcelo_neuro.telusko_ecommerce.repositories;
+
+import com.marcelo_neuro.telusko_ecommerce.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
