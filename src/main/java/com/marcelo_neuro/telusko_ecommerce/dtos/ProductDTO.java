03b581ce99cb40e6bf1997f1bcf5e8d3e405fb02
marcelo-neuro <marcelofurla0813@gmail.com>
2026-01-06 14:46:34 -0300

    Feat: created ProdutoDTO

---
+++ src/main/java/com/marcelo_neuro/telusko_ecommerce/dtos/ProductDTO.java
@@ -0,0 +1,51 @@
+package com.marcelo_neuro.telusko_ecommerce.dtos;
+
+import com.marcelo_neuro.telusko_ecommerce.entities.Product;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.BeanUtils;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ProductDTO {
+
+    private Long id;
+
+    @NotBlank(message = "Field 'name' must be filled.")
+    @Size(min = 3, max = 50, message = "Field 'name' must contain between 3 and 50 characters.")
+    private String name;
+
+    @NotBlank(message = "Field 'description' must be filled.")
+    @Size(min = 3, max = 120, message = "Field 'description' must contain between 3 and 120 characters.")
+    private String description;
+
+    @NotBlank(message = "Field 'brand' must be filled.")
+    @Size(min = 3, max = 50, message = "Field 'brand' must contain between 3 and 50 characters.")
+    private String brand;
+
+    @NotBlank(message = "Field 'category' must be filled.")
+    @Size(min = 3, max = 50, message = "Field 'category' must contain between 3 and 50 characters.")
+    private String category;
+
+    @NotNull(message = "Field 'price' must be filled.")
+    @Positive(message = "Field 'price', must be a positive value.")
+    private BigDecimal price;
+
+    @NotNull(message = "Field 'releaseDate' must be filled.")
+    private Date releaseDate;
+    private boolean productAvailable;
+
+    @NotNull(message = "Field 'stockQuantity' must be filled.")
+    @Positive(message = "Field 'stockQuantity', must be a positive value.")
+    private Integer stockQuantity;
+
+    public ProductDTO(Product entity) {
+        BeanUtils.copyProperties(entity, this);
+    }
+}
